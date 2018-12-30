30683ea49b4d99bd78777a6b1a951c73f65fac43
sean360 <sean@safety360.co.za>
2018-12-30 11:27:41 +0200

    delete playground file added

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,40 @@
+//set up mongo client
+//const MongoClient = require('mongodb').MongoClient;
+//desctrutioning
+const {MongoClient, ObjectID} = require('mongodb');
+
+//connect to database
+MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true }, (err, client) => {
+    if (err){
+        //use return here to stop the application on fail
+        return console.log('Unable to connect to local mongodb');
+    }
+    const db = client.db('TodoApp');
+    console.log('Connected to mongodb');
+    
+    //deleteMany
+    // db.collection('Todos').deleteMany({
+    //     text: 'Lizl for lunch'
+    // })
+    // .then((result) => {
+    //     console.log(result);
+    // })
+
+    //deleteOne
+    // db.collection('Todos').deleteOne({
+    //     text: 'Something to do'
+    // })
+    // .then((result) => {
+    //     console.log(result);
+    // });
+
+    //findOneAndDelete
+    db.collection('Todos').findOneAndDelete({
+        completed: false
+    })
+    .then((result) => {
+        console.log(result);
+    });
+
+    // client.close();
+});
