073340d62d3567db97bab5f3934ec25de0a579f0
sean360 <sean@safety360.co.za>
2018-12-31 10:45:57 +0200

    Add test for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,8 +5,16 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+    text: 'First test todo'
+}, {
+    text: 'Second test todo'
+}];
+
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+    Todo.deleteMany({}).then(() => {
+        return Todo.insertMany(todos)
+    }).then(() => done())
 })
 
 describe('Post/todos', () => {
@@ -25,7 +33,7 @@ describe('Post/todos', () => {
                     return done(err);
                 }
 
-                Todo.find()
+                Todo.find({text})
                 .then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
@@ -53,9 +61,21 @@ describe('Post/todos', () => {
                 //if all worked there should not be any info in the database so we check the lengts of the returned object and again return done
                 Todo.find()
                 .then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e) => done(e));
             });
     });
+});
+
+describe('GET /todos', () => {
+    it('should return all todos from db', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done)
+    })
 })
