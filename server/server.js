2ecd2485fb74f00b3b3c4bf5ddacc9bf6e2e9a8d
sean360 <sean@safety360.co.za>
2019-01-01 10:03:11 +0200

    Update the POST /todos response

--- server/server.js
+++ server/server.js
@@ -4,9 +4,9 @@ const bodyParser = require('body-parser');
 const ObjectID = require('mongodb').ObjectID;
 
 //Local Imports
-const {mongoose} = require('./db/mongoose');
-const {Todo} = require('./models/todo');
-const {User} = require('./models/user');
+const { mongoose } = require('./db/mongoose');
+const { Todo } = require('./models/todo');
+const { User } = require('./models/user');
 
 const app = express();
 
@@ -20,46 +20,43 @@ app.post('/todos', (req, res) => {
     const todo = new Todo({
         text: req.body.text
     });
-    
+
     todo.save()
-    .then((doc) => {
-        res.send(doc);
-    }, (err) => {
-        res.status(400).send(err);
-    })
+        .then((doc) => {
+            res.send(doc);
+        }, (err) => {
+            res.status(400).send(err);
+        })
 });
 
 //get all todos
 app.get('/todos', (req, res) => {
     Todo.find()
-    .then((todos) => {
-        res.send({
-            todos,
-            result: 'success'
+        .then((todos) => {
+            res.send({ todos });
+        }, (err) => {
+            res.send(err).status(400);
         });
-    }, (err) => {
-        res.send(err).status(400);
-    });
 });
 
 //get todo by id
 app.get('/todos/:id', (req, res) => {
-   
+
     const id = req.params.id;
 
-    if(!ObjectID.isValid(id)){
+    if (!ObjectID.isValid(id)) {
         return res.status(404).send();
     }
 
     Todo.findById(id)
-    .then((todo) => {
-        if (!todo){
-            return res.status(404).send();
-        }
-        res.send({todo});
-    }, (err) => {
-        res.status(400).send(err);
-    });
+        .then((todo) => {
+            if (!todo) {
+                return res.status(404).send();
+            }
+            res.send({ todo });
+        }, (err) => {
+            res.status(400).send(err);
+        });
 });
 
 //start up server
@@ -67,5 +64,5 @@ app.listen(port, () => {
     console.log(`App is up and running on ${port}`);
 });
 
-module.exports = {app};
+module.exports = { app };
 
