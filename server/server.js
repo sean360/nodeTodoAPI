35eaf6461a2e506e28ab5a9edb62d12b6b9bfc8f
sean360 <sean@safety360.co.za>
2019-01-01 10:07:24 +0200

    update to Invalid OBJECTID response

--- server/server.js
+++ server/server.js
@@ -45,7 +45,7 @@ app.get('/todos/:id', (req, res) => {
     const id = req.params.id;
 
     if (!ObjectID.isValid(id)) {
-        return res.status(404).send();
+        return res.status(404).send('Invalid ID');
     }
 
     Todo.findById(id)
